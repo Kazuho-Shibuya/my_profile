10f3f61466a54e015b419d9d11fca96acaa362ee
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-07 02:27:33 +0900

    send_mailメソッド追加

---
+++ app/mailers/inquiry_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class InquiryMailer < ApplicationMailer
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      from: 'system@example.com',
+      to: 'manager@example.com',
+      subject: 'お問い合わせ通知'
+    )
+  end
+end
