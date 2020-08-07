172b3ff56dad921df14366f654d0cf9ae0858938
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-08 03:37:35 +0900

    Inquiryモデルにemailカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_170559) do
+ActiveRecord::Schema.define(version: 2020_08_07_181845) do
 
   create_table "inquiries", force: :cascade do |t|
     t.string "name"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
