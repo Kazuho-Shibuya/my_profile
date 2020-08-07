9c86307d84a3bc8828b75dd91ebe33f84d7f3656
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-08 03:38:16 +0900

    Inquiryモデルにバリデーション追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Inquiry < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :message, presence: true
 end
