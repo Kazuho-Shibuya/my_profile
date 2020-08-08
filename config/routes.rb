e7456cc1a45455549e40a128cad74587d74a3b84
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-08 20:52:05 +0900

    contacts_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get  'contacts/new', to: 'contacts#new'
+  post 'contacts/new', to: 'contacts#create'
   root 'static_pages#top'
   get 'static_pages/portfolio'
   get 'static_pages/career'
-  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
