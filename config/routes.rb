e0b72655bc0f5cb7367571b9e5e3b56ae9a9ad6e
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-19 03:09:58 +0900

    静的ページ更新

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get 'static_pages/portfolio'
   get 'static_pages/career'
+  get 'static_pages/biography'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
